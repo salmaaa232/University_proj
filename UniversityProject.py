9ac89085f1007244df3a1141d1964a5cf5488f10
Twojays-FAG <netcompany6@gmail.com>
2025-02-27 09:03:55 +0200

    Add files via upload

---
+++ UniversityProject.py
@@ -0,0 +1,12 @@
+#Hello, this should be where we contribute our work as a team
+#Our team are: Mariam, Malak, Karim, Salma, and, finally, me, Kahrawi.
+class Person:
+    Person_counter = 0
+    def __init__(self, name: str, age: int, address: string, ID: int, phone_number: str, email: str):
+        self.name = name
+        self.age = age
+        self.address = address
+        self.ID = ID
+        self.phone_number = phone_number
+        self.email = email
+        Person_counter += 1
